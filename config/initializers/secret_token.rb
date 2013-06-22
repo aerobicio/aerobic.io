dae535d87c0fac24a4942903a4487a5431e5e7f7
Gareth Townsend <gareth.townsend@me.com>
2013-06-22 11:15:59 +1000

    Aerobic.io

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AerobicIo::Application.config.secret_key_base = '5a3eeda391d4627bdae85a0ac3c0bbd1f0c54d79cad67a799194732d543dc7c288bb92ffbc806922825b40fa6cab826f434398757d7e711abb1c9c26c11d8110'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AerobicIo::Application.config.session_store :cookie_store, key: '_aerobic_io_session'
