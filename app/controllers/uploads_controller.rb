083987619bc937585b1d9925d13986b6240c0d26
Gareth Townsend <gareth.townsend@me.com>
2013-06-23 14:47:52 +1000

    [SPIKE] Garmin FIT file uploads.
    File are uploaded and parsed into Fit object. They aren't saved anywhere and
    the user feedback is just a JS alert.
    
    Tested with a Garmin 500.

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,6 +1,22 @@
+require 'fit'
+
 class UploadsController < ApplicationController
-  layout false
+  layout 'garmin'
 
   def show
+  end
+
+  def create
+    fit = params[:activity]
+
+    fit = fit.lines.to_a[1..-1].join
+    fit = fit.lines.to_a[0..-2].join
+
+    fit = Base64.decode64(fit)
+    fit = Fit::File.read(StringIO.new(fit))
+
+    respond_to do |format|
+      format.json { render json: "OK" }
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 AerobicIo::Application.routes.draw do
-  resource :upload, only: :show
+  resource :upload, only: [ :show, :create ]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
