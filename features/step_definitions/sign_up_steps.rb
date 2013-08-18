1954f28c06aea811ceba553581e205dca45697f2
Gareth Townsend <gareth.townsend@me.com>
2013-08-19 08:47:47 +1000

    Add acceptance test around the bahaviour of the :sign_up feature flip.
    Tagged as @flip and @sign_up so that we can easily run all flips or just this
    flip.
    
    Written as a long running test that first turns the flip on, checks the
    behaviour, then turn the flip off and checks the behaviour. I've written it
    this way because the flip defaults to off and will be left in that state after
    this feature runs.

--- features/step_definitions/sign_up_steps.rb
+++ features/step_definitions/sign_up_steps.rb
@@ -21,3 +21,29 @@ end
 Then(/^I should be told I am not on the beta list$/) do
   page.should have_content("Email is not included in the beta list")
 end
+
+Given(/^the sign up feature is active$/) do
+  $rollout.activate(:sign_up)
+end
+
+When(/^I visit the sign up page$/) do
+  visit sign_in_path
+end
+
+Then(/^I should see the sign up link$/) do
+  page.should have_content(sign_up_link)
+  click_link(sign_up_link)
+  current_path.should == sign_up_path
+end
+
+Then(/^when the sign up feature is not active$/) do
+  $rollout.deactivate(:sign_up)
+end
+
+Then(/^I should not see the sign up link$/) do
+  page.should have_no_content(sign_up_link)
+end
+
+def sign_up_link
+  I18n.t("identities.new.submit")
+end
