1707c70c6af7ba30ace3191fd3409f552d5e4827
Gareth Townsend <gareth.townsend@me.com>
2013-08-19 19:55:31 +1000

    Feature flip sign up, remove beta user list.
    Instead of maintaining a list of beta users that are allowed to sign up, we
    now just turn the whole feature off. We can turn it on for ourselves, and then
    turn it back off to lock others out.
    
    This results in a better tested feature, that is usable going forward without
    make any changes to it.

--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -10,14 +10,7 @@ describe Identity do
 
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
-  it { should ensure_inclusion_of(:email).in_array(Identity::WHITE_LIST) }
 
-  # These tests are marked as pending becuase the white list negates them.
-  #
-  # When we remove the white listed email requirement we should re-instante
-  # these specs.
-  it "should allow 'spam+something@gmail.com' for email" do
-    pending "The removal of the white listed email requirement"
-  end
+  it { should allow_value("spam+something@gmail.com").for(:email) }
   it { should_not allow_value("blah").for(:email) }
 end
