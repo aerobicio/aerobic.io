961ff7bd0e4e872e5fd7d20f3c68620a886427f6
Gareth Townsend <gareth.townsend@me.com>
2013-08-20 07:00:02 +1000

    SwitchBoard: A wrapper for the rollout gem.
    SwitchBoard is a class that provides the ability to toggle features on and
    off. It wraps the rollout gem so that we do not expose the gems API to our
    code base.
    
    This will make it easier to upgrade or change feature flipping gems in the
    future should we desire to do so.
    
    It also serves as a single place to document all known feature flips in use
    within the application.

---
+++ lib/switch_board.rb
@@ -0,0 +1,31 @@
+# SwitchBoard is a class that provides the ability to toggle features on and
+# off. It wraps the rollout gem so that we do not expose the gems API to our
+# code base.
+#
+# This will make it easier to upgrade or change feature flipping gems in the
+# future should we desire to do so.
+#
+# It also serves as a single place to document all known feature flips in use
+# within the application.
+#
+class SwitchBoard
+  def initialize(rollout = $rollout)
+    @rollout = rollout
+  end
+
+  FEATURE_FLIPS = [:sign_up]
+
+  FEATURE_FLIPS.each do |flip_name|
+    define_method "activate_#{flip_name}" do
+      @rollout.activate(flip_name)
+    end
+
+    define_method "deactivate_#{flip_name}" do
+      @rollout.deactivate(flip_name)
+    end
+
+    define_method "#{flip_name}_is_active?" do
+      @rollout.active?(flip_name)
+    end
+  end
+end
