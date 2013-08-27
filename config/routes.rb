e0c09b4ad07509df802ab1d76203cb8d14f6ea5f
Gareth Townsend <gareth.townsend@me.com>
2013-08-27 20:40:22 +1000

    Don't generate identities/create route. It's not being used.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ AerobicIo::Application.routes.draw do
   get "auth/failure", to: redirect("/sign_in")
 
   resource :dashboard, only: [:show]
-  resources :identities, only: [:new, :create]
+  resources :identities, only: [:new]
   resources :sessions, only: [:create, :new]
 
   # The priority is based upon order of creation: first created -> highest priority.
