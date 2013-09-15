ca8c5a7504a259a147a0991b9302d51339399dd0
Gareth Townsend <gareth.townsend@me.com>
2013-09-16 07:29:46 +1000

    Revert "Try moving the ConsistencyFail enforcer to environment.rb"
    This reverts commit ecc88b3d962a2d38dbd05ccbd51f6da741c3168a.

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 AerobicIo::Application.initialize!
-
-require 'consistency_fail/enforcer'
-ConsistencyFail::Enforcer.enforce!
