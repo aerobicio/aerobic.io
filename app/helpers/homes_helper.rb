9ff13d443dbb88c80247fdaef01342eeb9936675
Justin Morris <justin@pixelbloom.com>
2013-09-21 14:29:38 +1000

    clean up NewHomesHelper

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -2,26 +2,28 @@
 # homes/view.
 #
 module HomesHelper
-
-  NEWSLETTER_FORM = {
-    action:       "http://aerobicio.createsend.com/t/t/s/itoy/",
-    field_name:   "cm-itoy-itoy",
-    field_id:     "itoy-itoy"
-  }
-
   # NewHomesHelper is the view class used in views/homes/show
   #
   class NewHomesHelper
+    include ActionView::Helpers::FormTagHelper
+
+    NEWSLETTER_FORM = {
+      action:       "http://aerobicio.createsend.com/t/t/s/itoy/",
+      field_name:   "cm-itoy-itoy",
+      field_id:     "itoy-itoy"
+    }
+
+    def email_signup_field
+      options = {
+        id: NEWSLETTER_FORM['field_id'],
+        placeholder: I18n.t("homes.show.landing.signup_form.field_placeholder"),
+        class: 'landing__signup__input'
+      }
+      email_field_tag(NEWSLETTER_FORM['field_name'], '', options)
+    end
+
     def newsletter_form_action
       NEWSLETTER_FORM['action']
-    end
-
-    def newsletter_form_field_name
-      NEWSLETTER_FORM['field_name']
-    end
-
-    def newsletter_form_field_id
-      NEWSLETTER_FORM['field_id']
     end
 
     def features
