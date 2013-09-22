8943b33ae91a985d1d6bf606d94b711d8d32a9e6
Gareth Townsend <gareth.townsend@me.com>
2013-09-22 13:34:12 +1000

    Following - Add pending step definitions for @following features.

---
+++ features/step_definitions/following_steps.rb
@@ -0,0 +1,51 @@
+Given(/^the following feature is off$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am following another member$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not be able to unfollow them$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am not following another member$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not be able to follow them$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^the following feature is on$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I follow another member$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see that fact in my activity feed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the member I followed should see that fact in their activity feed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^members who follow me should see that fact in their activity feed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I unfollow another member$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the member I unfollowed should not see that fact in their activity feed$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^members who follow me should not see that fact in their activity feed$/) do
+  pending # express the regexp above with the code you wish you had
+  endi
