36fdfb02743c54481113c4e2af7bdaa47db08f65
Justin Morris <justin@pixelbloom.com>
2013-09-29 18:02:22 +1000

    Set max-age on static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ AerobicIo::Application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.static_cache_control = "public, max-age=3600"
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
