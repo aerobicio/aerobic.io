308528627168f5341d0335a5be416aaaf5967cc3
Justin Morris <justin@pixelbloom.com>
2013-11-09 21:54:35 +1100

    following_link_for_member should bail unless following is active.

--- app/helpers/following_helper.rb
+++ app/helpers/following_helper.rb
@@ -2,6 +2,9 @@ module FollowingHelper
   include ActionView::Helpers::UrlHelper
 
   def following_link_for_member(member)
+    return unless $switch_board.following_active?
+
+    # TODO i18n these strings
     unless current_user == member
       if current_user.follows?(member)
         link_to("Unfollow", unfollow_path(member.id))
