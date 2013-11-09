d8c96b189a8df61e95cf29d43fcb56e199820ab1
Justin Morris <justin@pixelbloom.com>
2013-11-09 21:53:54 +1100

    Add AvatarHelper code.

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,10 +1,32 @@
 module AvatarHelper
-  def gravatar_for_member(member, size = 200)
-    gravatar_id   = Digest::MD5.hexdigest(member.user.email.downcase)
-    gravatar_path = "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+  GRAVATAR_URL = "http://gravatar.com/avatar/"
+  GRAVATAR_DIMENSIONS = {
+    normal: 64,
+    large:  164
+  }
 
-    content_tag :figure, class: "avatar" do
-      image_tag(gravatar_path, class: "avatar__image")
+  def gravatar_for_member(member, options = {})
+    options = {size: :normal, extra_classes: ''}.merge(options)
+
+    content_tag :figure, class: "avatar #{options[:extra_classes]}" do
+      image_tag(gravatar_url(member, options[:size]), class: "avatar__image")
     end
+  end
+
+  private
+
+  def dimension_for_size(size)
+    GRAVATAR_DIMENSIONS[size]
+  end
+
+  def gravatar_url(member, size)
+    gravatar_id   = gravatar_id(member)
+    gravatar_size = dimension_for_size(size)
+
+    "#{GRAVATAR_URL}#{gravatar_id}.png?s=#{gravatar_size}"
+  end
+
+  def gravatar_id(member)
+    Digest::MD5.hexdigest(member.email.downcase)
   end
 end
