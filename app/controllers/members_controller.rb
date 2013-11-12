52135c69cf0dd3d44c105937c482d93c1d8723f2
Gareth Townsend <gareth.townsend@me.com>
2013-11-12 18:03:45 +1100

    Break out follow_params on members controller for re-use.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -17,8 +17,7 @@ class MembersController < ApplicationController
     notice = nil
 
     ActiveRecord::Base.transaction do
-      result = FollowMember.perform(member_id: current_user.id,
-                                    followed_id: params[:id])
+      result = FollowMember.perform(follow_params)
       notice = result.notice
       raise ActiveRecord::Rollback unless result.success?
     end
@@ -30,9 +29,7 @@ class MembersController < ApplicationController
     notice = nil
 
     ActiveRecord::Base.transaction do
-      result = UnFollowMember.perform(member_id: current_user.id,
-                                      followed_id: params[:id])
-
+      result = UnFollowMember.perform(follow_params)
       notice = result.notice
       raise ActiveRecord::Rollback unless result.success?
     end
@@ -41,6 +38,13 @@ class MembersController < ApplicationController
   end
 
   private
+
+  def follow_params
+    {
+      member_id: current_user.id,
+      followed_id: params[:id],
+    }
+  end
 
   def ensure_following_is_active
     unless $switch_board.following_active?
