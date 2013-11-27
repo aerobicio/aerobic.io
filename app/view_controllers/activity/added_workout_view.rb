0ec3a52ec84da8c2a1eee4fd43b1827587abf4e5
Gareth Townsend <gareth.townsend@me.com>
2013-11-27 18:54:32 +1100

    Add Activity::AddedWorkoutView ViewController.
    Along with specs that prove that the correct names are being used when rendering
    the partial.

---
+++ app/view_controllers/activity/added_workout_view.rb
@@ -0,0 +1,41 @@
+class Activity
+  class AddedWorkoutView
+    def initialize(current_member, added_workout)
+      @current_member = current_member
+      @added_workout = added_workout
+    end
+
+    def cache_key
+      [
+        @added_workout.cache_key,
+        @added_workout.activity_workout.cache_key,
+      ].map(&:to_s).join(":")
+    end
+
+    def title
+      if @current_member == member
+        "You did a workout:"
+      else
+        "#{member.name} did a workout:"
+      end
+    end
+
+    def duration
+      "#{(workout.active_duration / 1000 / 60)} minutes"
+    end
+
+    def distance
+      "#{(workout.distance / 100000.0).round(2)}km"
+    end
+
+    private
+
+    def member
+      @added_workout.activity_user
+    end
+
+    def workout
+      @added_workout.activity_workout
+    end
+  end
+end
