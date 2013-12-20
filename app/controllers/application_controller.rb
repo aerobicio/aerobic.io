3418d7a022209e7ac5b6b5339ae01241acc9b8ed
Justin Morris <justin@pixelbloom.com>
2013-12-20 17:36:22 +1100

    Don't access the ENV from ERB, add application helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user
+  helper_method :current_user, :home_page_path
 
   layout "authenticated"
 
