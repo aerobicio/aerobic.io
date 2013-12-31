90d2cb0edf52949300cd533a88048dede8b3a6a9
Justin Morris <justin@pixelbloom.com>
2013-12-31 12:22:06 +1100

    Remove #activity.

--- features/step_definitions/workout_steps.rb
+++ features/step_definitions/workout_steps.rb
@@ -5,10 +5,8 @@ end
 When(/^I visit the workout page from the activity feed$/) do
   visit dashboard_path
 
-  within "#activity" do
-    page_has_workout
-    click_link(I18n.t("activity.workout.title.first_person"))
-  end
+  page_has_workout
+  click_link(I18n.t("activity.workout.title.first_person"))
 end
 
 Then(/^I should see my workout$/) do
