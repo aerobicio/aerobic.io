ba28b7f9e66b1bb8268a5f66078114bc7a9b4a2b
Justin Morris <justin@pixelbloom.com>
2013-12-31 14:08:04 +1100

    Steps.

--- features/step_definitions/upload_from_garmin_steps.rb
+++ features/step_definitions/upload_from_garmin_steps.rb
@@ -1,3 +1,7 @@
+When(/^I visit the upload page$/) do
+  visit upload_path
+end
+
 When(/^I add a workout$/) do
   add_workout
 end
@@ -5,4 +9,9 @@ end
 Then(/^I should see workout information on my dashboard$/) do
   visit dashboard_path
   page.should have_content "Distance: 41.32km"
+end
+
+Then(/^I should see a message telling me that I have no devices$/) do
+  binding.pry
+  page.should have_content "We couldn't find any devices!"
 end
