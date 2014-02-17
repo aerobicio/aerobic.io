81f60c5fd780d48b34d206f3978212e0f4a815c6
Gareth Townsend <gareth.townsend@me.com>
2014-02-17 18:51:41 +1100

    Add stub to cover the existing_member_workouts method.
    Doesn't actually test the logic, just that the method doesn't explode when
    give the correct object.

--- spec/controllers/uploads_controller_spec.rb
+++ spec/controllers/uploads_controller_spec.rb
@@ -4,6 +4,17 @@ describe UploadsController do
   let(:user) { double(User, id: 42) }
   let(:user_workouts) { double() }
 
+  let(:workout) do
+    double(:workout, attributes: { distance: 123,
+                                   active_duration: 123,
+                                   duration: 123
+                                 },
+                     distance: 123,
+                     active_duration: 123,
+                     duration: 123
+          )
+  end
+
   before do
     session[:user_id] = 1
     User.stub(:find).and_return(user)
@@ -12,7 +23,8 @@ describe UploadsController do
   describe "#show" do
     before do
       user.should_receive(:workouts) { user_workouts }
-      user_workouts.should_receive(:load) { [] }
+      user_workouts.should_receive(:load) { [workout] }
+
       get :show
     end
 
