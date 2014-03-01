6a980e11a67f46d0a5a25247afc4b96ce9afec13
Justin Morris <justin@pixelbloom.com>
2014-03-01 13:42:50 +1100

    Add Teegs to private beta list.

--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -1,9 +1,12 @@
 require_relative "redis.rb"
 
 STAFF_EMAILS = ["gareth.townsend@me.com", "desk@pixelbloom.com"]
+PRIVATE_BETA_EMAILS = [
+  "neikinu@gmail.com",
+]
 
 $rollout = Rollout.new($redis)
 
 $rollout.define_group(:staff) do |user|
-  STAFF_EMAILS.include?(user.email)
+  STAFF_EMAILS.concat(PRIVATE_BETA_EMAILS).include?(user.email)
 end
