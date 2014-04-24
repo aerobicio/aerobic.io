e995b4e499be4d70bf0d5d0e4c158064b541195b
Gareth Townsend <gareth.townsend@me.com>
2014-04-24 16:21:06 +1000

    Allow beta users to sign up if they know the token.

--- spec/controllers/identities_controller_spec.rb
+++ spec/controllers/identities_controller_spec.rb
@@ -16,10 +16,41 @@ describe IdentitiesController do
     context 'when sign up is not active' do
       before do
         $switch_board.stub(:sign_up_active?) { false }
-        get :new
       end
 
-      it { should redirect_to(sign_in_path) }
+      context 'and no token param is set' do
+        before do
+          get :new
+        end
+
+        it { should redirect_to(sign_in_path) }
+      end
+
+      context 'and ENV["BETA_TOKEN"] is not set' do
+        before do
+          ENV.stub('BETA_TOKEN') { nil }
+          get :new
+        end
+
+        it { should redirect_to(sign_in_path) }
+      end
+
+      context 'and the token param is set to an unrecognised value' do
+        before do
+          get :new, token: ENV['BETA_TOKEN'].dup.reverse!
+        end
+
+        it { should redirect_to(sign_in_path) }
+      end
+
+      context 'and the token param is set to ENV["BETA_TOKEN"]' do
+        before do
+          get :new, token: ENV['BETA_TOKEN']
+        end
+
+        it { should respond_with(:success) }
+        it { should render_template(:new) }
+      end
     end
   end
 end
