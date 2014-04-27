b0e3b6b9c446e25a91a2a051d5db763423a78470
Justin Morris <justin@pixelbloom.com>
2014-04-28 07:27:53 +1000

    Migrate

---
+++ app/models/comment.rb
@@ -0,0 +1,8 @@
+class Comment < ActiveRecord::Base
+  include ActsAsCommentable::Comment
+
+  belongs_to :commentable, :polymorphic => true
+  belongs_to :user
+
+  default_scope -> { order('created_at ASC') }
+end
