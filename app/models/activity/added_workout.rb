3486f3489913a793edacadd7a76ed8a8ab0e350b
Justin Morris <justin@pixelbloom.com>
2014-04-28 07:37:25 +1000

    Fixing linters

--- app/models/activity/added_workout.rb
+++ app/models/activity/added_workout.rb
@@ -1,6 +1,8 @@
 require 'activity'
 
 class Activity
+  # ActiveRecord object for storing Added Workout Activities
+  #
   class AddedWorkout < Activity
     acts_as_commentable
   end
--- app/models/activity/followed_user.rb
+++ app/models/activity/followed_user.rb
@@ -1,6 +1,8 @@
 require 'activity'
 
 class Activity
+  # ActiveRecord object for storing Following User Activities
+  #
   class FollowedUser < Activity
     acts_as_commentable
   end
